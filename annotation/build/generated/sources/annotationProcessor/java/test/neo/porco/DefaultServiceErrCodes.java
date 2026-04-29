b4667e06e0c7c818237ac696bc2cbf22c4b37474
Neodreamtale <191982657@qq.com>
2026-04-29 16:23:04 +0800

    Delete annotation/build directory

--- annotation/build/generated/sources/annotationProcessor/java/test/neo/porco/DefaultServiceErrCodes.java
+++
@@ -1,4 +0,0 @@
-package neo.porco;
-
-public class DefaultServiceErrCodes {
-}
--- annotation/build/generated/sources/annotationProcessor/java/test/neo/porco/LoginServiceErrCodes.java
+++
@@ -1,9 +0,0 @@
-package neo.porco;
-
-import java.lang.String;
-
-public class LoginServiceErrCodes {
-  public static final String ERROR_USER_NOT_FOUND = "Error_USER_NOT_FOUND";
-
-  public static final String ERROR_INVALID_PASSWORD = "Error_INVALID_PASSWORD";
-}
